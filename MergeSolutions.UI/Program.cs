42140ae0cc75ad5b90da3daef6f403a563c1582d
Oleg Bessonov <vodesoft@users.noreply.github.com>
2022-09-26 09:21:30 +0400

    Exception handling is added

--- MergeSolutions.UI/Program.cs
+++ MergeSolutions.UI/Program.cs
@@ -1,5 +1,7 @@
+using System.Runtime.CompilerServices;
 using MergeSolutions.Core.Services;
 using Microsoft.Extensions.DependencyInjection;
+// ReSharper disable LocalizableElement
 
 namespace MergeSolutions.UI
 {
@@ -7,24 +9,56 @@ namespace MergeSolutions.UI
     {
         public static IServiceProvider ServiceProvider { get; private set; } = null!;
 
+        public static void ShowExceptionMessage(string message, Exception? exception)
+        {
+            MessageBox.Show(@$"{message} 
+
+{exception?.Message}
+{exception?.InnerException?.Message}",
+                "Exception",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error);
+        }
+
+        private static void ApplicationOnThreadException(object sender, ThreadExceptionEventArgs args)
+        {
+            ShowExceptionMessage("Application thread unhandled exception.", args.Exception);
+        }
+
+        private static void CurrentDomainOnUnhandledException(object sender, UnhandledExceptionEventArgs args)
+        {
+            var exception = args.ExceptionObject as Exception ?? args.ExceptionObject as RuntimeWrappedException;
+            ShowExceptionMessage("CurrentDomain unhandled exception.", exception);
+        }
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         private static void Main(string[] args)
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
+            try
+            {
+                // To customize application configuration such as set high DPI settings or default font,
+                // see https://aka.ms/applicationconfiguration.
+                ApplicationConfiguration.Initialize();
+                AppDomain.CurrentDomain.UnhandledException += CurrentDomainOnUnhandledException;
+                Application.ThreadException += ApplicationOnThreadException;
+                Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
 
-            var services = new ServiceCollection();
-            services.AddTransient<MainForm>();
-            services.AddSingleton<IMergeSolutionsService, MergeSolutionService>();
-            services.AddSingleton<ISolutionService, SolutionService>();
-            services.AddTransient<IStartup>(_ => new Startup(args?.ElementAtOrDefault(0)));
-            ServiceProvider = services.BuildServiceProvider();
+                var services = new ServiceCollection();
+                services.AddTransient<MainForm>();
+                services.AddSingleton<IMergeSolutionsService, MergeSolutionService>();
+                services.AddSingleton<ISolutionService, SolutionService>();
+                services.AddTransient<IStartup>(_ => new Startup(args.ElementAtOrDefault(0)));
+                ServiceProvider = services.BuildServiceProvider();
 
-            Application.Run(ServiceProvider.GetRequiredService<MainForm>());
+                Application.Run(ServiceProvider.GetRequiredService<MainForm>());
+            }
+            catch (Exception e)
+            {
+                ShowExceptionMessage("Unhandled application exception.", e);
+            }
         }
     }
 }
